64212fc87399fedb5ff18f9d27d3b58f65fe307c
Sam Parsons <samparsons269@gmail.com>
2021-05-23 15:10:09 -0400

    added cases for missing argument and single commands

--- test/plugin.test.js
+++ test/plugin.test.js
@@ -2,9 +2,28 @@ const plugin = require('../plugin');
 
 describe('snowpack run script plugin', () => {
   test('invokes plugin and returns correctly shaped object without error', () => {
-    const obj = plugin();
+    const obj = plugin({}, { cmd: 'ls' });
     expect(obj.name).toEqual('snowpack-plugin-run');
     expect(typeof obj.run).toEqual('function');
+  });
+
+  test('returns error when invoked without options', () => {
+    const err = plugin();
+    expect(err.message).toEqual('Argument required for plugin');
+  });
+
+  test('invokes plugin with one single command', () => {
+    const obj = plugin(
+      {},
+      {
+        cmd: 'echo "this is a line"',
+      }
+    );
+    const log = jest.fn();
+    return obj.run({ log }).then((data) => {
+      expect(log).toHaveBeenCalled();
+      expect(data[0].stdout === 'this is a line');
+    });
   });
 
   test('invokes plugin with two cli commands', () => {
